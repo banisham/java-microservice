d334fe5965ecc354a2c2e8e881efc385c6a78d88
Banisha M <banisha.m@gmail.com>
2020-03-23 13:03:18 +0800

    Deleted files

--- src/main/java/com/infra/dev/infradevaccess/DemoApplication.java
+++ src/main/java/com/infra/dev/infradevaccess/DemoApplication.java
@@ -2,45 +2,46 @@ package com.infra.dev.infradevaccess;
 
 import com.infra.dev.infradevaccess.interceptor.GenericInterceptor;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
 @EnableEurekaClient
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
 
-	@Bean
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
     public ModelMapper modelMapper() {
-    return new ModelMapper();
-   }
+        return new ModelMapper();
+    }
 
-   @Bean
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {        
+        return new WebMvcConfigurerAdapter() {
             @Override
             public void addInterceptors(InterceptorRegistry registry) {
                 registry.addInterceptor(new GenericInterceptor()).addPathPatterns("/api/**")
-                .excludePathPatterns("/v2/api-docs","/configuration/ui","/swagger-resources/**",
-                "/configuration/**","/swagger-ui.html","/");
+                        .excludePathPatterns("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
+                                "/configuration/**", "/swagger-ui.html", "/");
             }
         };
     }
 
     @Bean
-    public static PropertySourcesPlaceholderConfigurer addPropertyConfig(){
+    public static PropertySourcesPlaceholderConfigurer addPropertyConfig() {
         PropertySourcesPlaceholderConfigurer p = new PropertySourcesPlaceholderConfigurer();
         YamlPropertiesFactoryBean factoryBean = new YamlPropertiesFactoryBean();
         factoryBean.setResources(new ClassPathResource("swagger-config.yaml"));
@@ -48,5 +49,8 @@ public class DemoApplication {
         return p;
 
     }
+
+
+}
 
 }
